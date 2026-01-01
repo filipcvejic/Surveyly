ca16ed56e1b448338e2e3a060de7e8c63478b1ad
filipcvejic <l3g4l.05@gmail.com>
2026-01-02 00:22:26 +0100

    fixed sqlc import name typo mistake

--- repositories/user.go
+++ repositories/user.go
@@ -2,21 +2,21 @@ package repositories
 
 import (
 	"context"
-	sqlc2 "github.com/filipcvejic/surveyly/db/sqlc"
+	"github.com/filipcvejic/surveyly/db/sqlc"
 	"github.com/google/uuid"
 	"time"
 )
 
 type UserRepository struct {
-	queries *sqlc2.Queries
+	queries *sqlc.Queries
 }
 
-func NewUserRepository(queries *sqlc2.Queries) *UserRepository {
+func NewUserRepository(queries *sqlc.Queries) *UserRepository {
 	return &UserRepository{queries: queries}
 }
 
-func (r *UserRepository) CreateUser(ctx context.Context, email, username, passwordHash string) (sqlc2.User, error) {
-	return r.queries.CreateUser(ctx, sqlc2.CreateUserParams{
+func (r *UserRepository) CreateUser(ctx context.Context, email, username, passwordHash string) (sqlc.User, error) {
+	return r.queries.CreateUser(ctx, sqlc.CreateUserParams{
 		ID:           uuid.New(),
 		Email:        email,
 		Username:     username,
@@ -25,10 +25,10 @@ func (r *UserRepository) CreateUser(ctx context.Context, email, username, passwo
 	})
 }
 
-func (r *UserRepository) GetUserByID(ctx context.Context, id uuid.UUID) (sqlc2.User, error) {
+func (r *UserRepository) GetUserByID(ctx context.Context, id uuid.UUID) (sqlc.User, error) {
 	return r.queries.GetUserByID(ctx, id)
 }
 
-func (r *UserRepository) GetUserByEmail(ctx context.Context, email string) (sqlc2.User, error) {
+func (r *UserRepository) GetUserByEmail(ctx context.Context, email string) (sqlc.User, error) {
 	return r.queries.GetUserByEmail(ctx, email)
 }
